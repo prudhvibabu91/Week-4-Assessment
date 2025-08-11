fc947b4915cbfbb06db2330fcd209fd4f9afbdb8
prudhvi babu <prudhvibabu9@gmail.com>
2025-08-11 18:44:57 +0530

    Update Week 4 Assessment_Qn9.sql

--- Week 4 Assessment_Qn9.sql
+++ Week 4 Assessment_Qn9.sql
@@ -2,21 +2,18 @@ CREATE OR ALTER PROCEDURE DeleteEmployee
     @employeeId INT
 AS
 BEGIN
-    -- Step 1: Remove this employee as a manager for others
+    
     UPDATE Employee
     SET managerId = NULL
     WHERE managerId = @employeeId;
 
-    -- Step 2: Delete the employee
     DELETE FROM Employee 
     WHERE employeeId = @employeeId;
 
-    PRINT 'Record Deleted';
+    PRINT 'Record Deleted successfully';
 END;
 GO
-
--- Sample execution
 EXEC DeleteEmployee @employeeId = 1;
 
--- Check remaining records
 SELECT * FROM Employee;
+
